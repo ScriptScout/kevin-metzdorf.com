045892d593c9ba4634e691504ad99d008df87c1e
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-08-12 16:49:11 +0200

    #

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  icons: string[] = ['angular.png', 'typescript.png', 'javascript.png', 'html.png', 'firebase.png', 'git.png', 'css.png', 'rest_api.png', 'scrum.png', 'material_design.png'];
+  icons_text: string[] = ['Angular', 'Typescript', 'Javascript', 'Html', 'Firebase', 'Git', 'Css', 'Rest_api', 'Scrum', 'Material design'];
 }
