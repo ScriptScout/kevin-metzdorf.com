6a1b85772be4141fc5771e86d6b13d894bd72feb
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-08-12 12:04:30 +0200

    about me desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MainComponent } from './main/main.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MainComponent } from './main/main.component';
     HeaderComponent,
     HomeComponent,
     AboutMeComponent,
-    MainComponent
+    MainComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
