d5ee48e704eda7625d4b36296bf702aea60f179f
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-08-21 12:27:40 +0200

    added set menu active while scroll

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,22 +7,46 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  activeSection: string = '';
   menuOpen: boolean = false;
 
   ngOnInit() {
-    this.listenToMenu();
+    this.menuEventListener();
   }
 
   openMenu() {
     this.menuOpen = !this.menuOpen;
   }
 
-  listenToMenu() {
-    const menu = document.querySelectorAll('a.hover_underline_animation');
+  menuEventListener() {
+    const menu = document.querySelectorAll('a.nav_link');
     menu.forEach(link => {
       link.addEventListener('click', (event) => {
         this.openMenu();
+        this.resetMenuActive();
+        link.classList.add('active');
       });
     });
+  }
+
+  resetMenuActive() {
+    const menu = document.querySelectorAll('a.nav_link');
+    menu.forEach(link => {
+      link.classList.remove('active');
+    });
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const home = document.getElementById('home')!.getBoundingClientRect().top;
+    const about = document.getElementById('about_me')!.getBoundingClientRect().top;
+    const skills = document.getElementById('skills')!.getBoundingClientRect().top;
+    const portfolio = document.getElementById('portfolio')!.getBoundingClientRect().top;
+    const contact = document.getElementById('contact')!.getBoundingClientRect().top;
+    if (home <= 0 && home > -164) this.activeSection = '';
+    if (about < 663 && about > -53) this.activeSection = 'about';
+    if (skills < 585 && skills > 86) this.activeSection = 'skills';
+    if (portfolio < 769 && portfolio > -500) this.activeSection = 'portfolio';
+    if (contact < 855 && contact > 19) this.activeSection = 'contact';
   }
 }
