0694f6599705b7412130053a92e2c10d64623f0a
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-08-21 11:16:03 +0200

    working on setting menu elements active

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,20 @@ export class HeaderComponent {
 
   menuOpen: boolean = false;
 
+  ngOnInit() {
+    this.listenToMenu();
+  }
+
   openMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  listenToMenu() {
+    const menu = document.querySelectorAll('a.hover_underline_animation');
+    menu.forEach(link => {
+      link.addEventListener('click', (event) => {
+        this.openMenu();
+      });
+    });
   }
 }
