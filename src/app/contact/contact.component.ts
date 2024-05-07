4acdab28e173935dd31ddf65d03e523575fa6ec7
Scriptscout <kevin.metzdorf@icloud.com>
2024-05-07 18:20:26 +0200

    Add custom checkbox and update form styling
    In this commit, the style of the contact form has been updated for improved readability. Additional changes include the implementation of a custom checkbox in the form for a more appealing user interface.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,43 +10,62 @@ export class ContactComponent {
 
   message: string = 'Send message :)';
   contactForm: FormGroup;
-  isSending: boolean = false;
+  isSubmited: boolean = false;
+  isSent: boolean = false;
+  checkboxValue: boolean = false;
+
 
   constructor() {
     this.contactForm = new FormGroup({
       name: new FormControl({ value: '', disabled: false }, Validators.required),
       email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-      text: new FormControl({ value: '', disabled: false }, Validators.required)
+      text: new FormControl({ value: '', disabled: false }, Validators.required),
+      privacy: new FormControl(false)
     });
   }
 
-  async onSubmit() {
-    this.disableForm();
-    this.isSending = true;
 
+  async onSubmit() {
+    if (this.contactForm.valid && this.contactForm.get('privacy')?.value) {
+      this.disableForm();
+      try {
+        const response = await fetch('https://kevin-ammerman.com/send_mail/send_mail.php', {
+          method: 'POST',
+          body: this.setFormData()
+        });
+        this.resetAndNotify();
+        this.isSent = true;
+        if (response.ok) this.message = 'Success! :)';
+      } catch (error) {
+        console.error('Error sending message:', error);
+        this.message = `Oops! Something went wrong`;
+      }
+    } else {
+      this.showValidationErrorMsg()
+    }
+  }
+
+
+  setFormData() {
     let fd = new FormData();
     fd.append('name', this.contactForm.value.name);
     fd.append('email', this.contactForm.value.email);
     fd.append('message', this.contactForm.value.text);
+    return fd;
+  }
 
-    try {
-      const response = await fetch('https://kevin-ammerman.com/send_mail/send_mail.php', {
-        method: 'POST',
-        body: fd
-      });
 
-      if (!response.ok) throw new Error(`Server responded with status: ${response.status}`);
-      this.message = 'Success! :)';
+  showValidationErrorMsg() {
+    this.isSubmited = true;
+    setTimeout(() => this.isSubmited = false, 2000);
+  }
 
-    } catch (error) {
-      console.error('Error sending message:', error);
-      this.message = `Oops! Something went wrong`;
-    }
 
+  resetAndNotify() {
     setTimeout(() => {
       this.resetForm();
       this.enableForm();
-      this.isSending = false;
+      this.isSent = false;
     }, 5000);
 
     setTimeout(() => this.message = 'Send message :)', 12000);
@@ -57,7 +76,8 @@ export class ContactComponent {
     this.contactForm.reset({
       name: '',
       email: '',
-      text: ''
+      text: '',
+      privacy: false
     })
   }
 
