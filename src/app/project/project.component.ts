1e4237a9941bde59ba6afcea754afb4449256d7c
Scriptscout <kevin.metzdorf@icloud.com>
2024-05-08 08:32:38 +0200

    Update project component
    The project component was updated to improve image descriptions, amend project descriptions, and add project details including github links and live project URLs.

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -14,11 +14,13 @@ export class ProjectComponent {
       description: 'Optimierung der Performance, Sicherheit und Stabilität durch Analyse, Fehlerbehebung, Code-Review und Implementierung von Best Practices. Anpassung und Erweiterung des Shops nach Kundenwunsch, Implementierung neuer Funktionen und Integration von Drittanbieter-Apps.',
       image: 'decocino.jpeg',
       animation: 'fade-right',
+      github: 'https://github.com/ScriptScout',
+      live: 'https://decocino.com',
     },
     {
       name: 'El Polo Locco',
       technologies: 'JavaScript | HTML | CSS | OOP',
-      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      description: 'Ein einfaches Jump-and-Run-Spiel, das auf einem objektorientierten Ansatz basiert. Hilf Pepe dabei, Münzen und Tabasco-Salsa zu finden, um gegen die verrückte Henne zu kämpfen.',
       image: 'el_pollo_loco.webp',
       animation: 'fade-left',
       github: 'https://github.com/ScriptScout',
@@ -30,8 +32,17 @@ export class ProjectComponent {
       description: 'Sicherstellung der Warenverfügbarkeit durch permanente Bestandskontrolle, Optimierung der Lagerhaltung durch MHD-Prüfung und -management, sowie proaktive Warennachbestellung und -anmeldung. Kommunikation und Klärung von Lieferfragen mit Logistikpartnern zur reibungslosen Warenanlieferung und Sicherstellung eines hohen Kundenzufriedenheitsniveaus.',
       image: 'ready2drink.jpeg',
       animation: 'fade-right',
-      github: '',
+      github: 'https://github.com/ScriptScout',
       live: 'https://ready2drink.de'
-    }
+    },
+    {
+      name: ' JTL-Wawi-Experte für die Optimierung von Importprozessen: JTL Wawi Experte ',
+      technologies: 'JTL | Shopify | Windows Server',
+      description: 'Durch die Entwicklung eines PowerShell-Skripts, die Integration mit dem Windows Task Scheduler, die Verwendung der JTL-Ameise und die Einrichtung der Shopify-Integration wurde ein umfassender und automatisierter Datenimport- und Synchronisierungsprozess implementiert.',
+      image: 'suppotheke.jpeg',
+      animation: 'fade-left',
+      github: 'https://github.com/ScriptScout',
+      live: 'https://suppotheke.de'
+    },
   ]
 }
