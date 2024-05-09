5e3104c825b701a21f4dd2b9e0a1516c0daa9057
Scriptscout <kevin.metzdorf@icloud.com>
2024-05-09 13:33:08 +0200

    Remove package-lock.json file

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-contact',
@@ -14,8 +15,7 @@ export class ContactComponent {
   isSent: boolean = false;
   checkboxValue: boolean = false;
 
-
-  constructor() {
+  constructor(private firestore: AngularFirestore) {
     this.contactForm = new FormGroup({
       name: new FormControl({ value: '', disabled: false }, Validators.required),
       email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
@@ -24,18 +24,17 @@ export class ContactComponent {
     });
   }
 
-
   async onSubmit() {
     if (this.contactForm.valid && this.contactForm.get('privacy')?.value) {
       this.disableForm();
+      const formData = this.contactForm.value;
+
       try {
-        const response = await fetch('https://kevin-ammerman.com/send_mail/send_mail.php', {
-          method: 'POST',
-          body: this.setFormData()
-        });
+        // Change PHP mail sending function to Firestore
+        await this.firestore.collection('contacts').add(formData);
         this.resetAndNotify();
+        this.message = 'Success!';
         this.isSent = true;
-        if (response.ok) this.message = 'Success! :)';
       } catch (error) {
         console.error('Error sending message:', error);
         this.message = `Oops! Something went wrong`;
@@ -45,21 +44,10 @@ export class ContactComponent {
     }
   }
 
-
-  setFormData() {
-    let fd = new FormData();
-    fd.append('name', this.contactForm.value.name);
-    fd.append('email', this.contactForm.value.email);
-    fd.append('message', this.contactForm.value.text);
-    return fd;
-  }
-
-
   showValidationErrorMsg() {
     this.isSubmited = true;
     setTimeout(() => this.isSubmited = false, 2000);
   }
-
 
   resetAndNotify() {
     setTimeout(() => {
@@ -71,7 +59,6 @@ export class ContactComponent {
     setTimeout(() => this.message = 'Send message :)', 12000);
   }
 
-
   resetForm() {
     this.contactForm.reset({
       name: '',
@@ -81,11 +68,9 @@ export class ContactComponent {
     })
   }
 
-
   disableForm() {
     this.contactForm.disable();
   }
-
 
   enableForm() {
     this.contactForm.enable();
